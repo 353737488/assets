5daa95d5b84e7cfdba0c063c34105be0ba698836
hunzsig <mzyhaohaoren@qq.com>
2022-06-04 11:02:18 +0800

    feat(*): update

--- war3mapUI/singluar_set/scripts/buff.lua
+++ war3mapUI/singluar_set/scripts/buff.lua
@@ -59,11 +59,16 @@ _singluarSetBuff = {
             mpCur = "<当前>魔法",
             punishCur = "<当前>硬直",
             weightCur = "<当前>负重",
+
+            _leapBuff = "冲锋中",
+            _crackFlyBuff = "被击飞",
         }
         -- 图标转接
         stage.buff_turner._icon = {
             attackSpaceBase = "attackSpace",
             attackRangeAcquire = "attackRange",
+            _leapBuff = "leap",
+            _crackFlyBuff = "crackFly",
         }
         enchant.types.forEach(function(key, value)
             stage.buff_turner._name["e_" .. key] = value.label .. '<强化>'
